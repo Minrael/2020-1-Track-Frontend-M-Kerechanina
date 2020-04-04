e331c256f1185187a4e1cc0f4905ad36e730113b
Maria <maria150@mail.ru>
2020-04-05 01:32:09 +0300

    fix

--- src/components/MessageBox.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function messageToBox (messageText, key) {
-    return (<p key = {key} className = 'message' dangerouslySetInnerHTML={{__html:messageText}}></p>)
-}
