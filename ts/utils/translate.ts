ab440cccb9b07ffc0545fdb35e5da09a8e2ed9af
Maria <maria150@mail.ru>
2020-04-18 00:05:55 +0300

    hw 8 v.0

---
+++ ts/utils/translate.ts
@@ -0,0 +1,32 @@
+import { TApiResponse } from './types' 
+import { APIKEY, URLDOMEN, URLPATH } from './constants'
+import { someError, NetError } from './errors'
+//import { langValidate } from './inputValidate'
+
+const fetch = require('node-fetch');
+if (!globalThis.fetch) {
+	globalThis.fetch = fetch;
+}
+
+let status = (response: Response) => {
+	if (response.status !== 200) {
+		return Promise.reject(new NetError(response.statusText))
+	}
+	return Promise.resolve(response)
+}
+
+let toJson = (response: Response): Promise<TApiResponse> => {
+	return response.json()
+}
+
+export default function translate (text: String[], lang: string, defLang:boolean=false): Promise<TApiResponse>{
+	//let data = langValidate(text, lang, defLang)
+	let URLPRMS = `?key=${APIKEY}&text=${text}&lang=${lang}&format=plain`
+	let API = URLDOMEN + URLPATH + URLPRMS;
+
+	return fetch(API)
+		.then(status)
+		.catch(someError)
+		.then(toJson)
+		.catch(someError)		
+}
